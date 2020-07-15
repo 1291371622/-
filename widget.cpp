cae74008f85fd9cfb0224e4f1e1a1ed463f0cf1a
1291371622 <49634872+1291371622@users.noreply.github.com>
2020-07-15 16:55:13 +0800

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -19,7 +19,7 @@ Widget::~Widget()
 void Widget::on_pushButton_clicked()
 {
     QByteArray url="http://tsn.baidu.com/text2audio?";
-    url.append(QString("&lan=zh&ctp=1&cuid=74-E5-F9-C0-FB-0D&ctp=1&tok=24.ceaa23bc28123afef779d02be61e43f3.2592000.1597328991.282335-21231900&vol=9&per=0&spd=5&pit=5&aue=3"));
+    url.append(QString("&lan=zh&ctp=1&cuid=xxxx&ctp=1&tok=xxxx&vol=9&per=0&spd=5&pit=5&aue=3"));
     url.append("&tex=");
     url.append(QUrl::toPercentEncoding(ui->textEdit->toPlainText()));
     qDebug()<<url;
